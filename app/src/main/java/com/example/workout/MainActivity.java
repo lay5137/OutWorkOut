3725af6d774febce31db2d3412ccf636f6b726c2
ChanGyuLee <lay5137@naver.com>
2024-06-05 00:32:06 +0900

    .

--- app/src/main/java/com/example/workout/MainActivity.java
+++ app/src/main/java/com/example/workout/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.workout;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +16,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ImageView calendarIcon = findViewById(R.id.calendar_icon);
+        Button BodyWeightExercise = findViewById(R.id.bodyweight_exercise);
         calendarIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, CalendarActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        BodyWeightExercise.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ExerciseSelectionActivity.class);
                 startActivity(intent);
             }
         });
